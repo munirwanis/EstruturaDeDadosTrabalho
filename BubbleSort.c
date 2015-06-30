88b0a18e45b03dccc524e5326cf244bfc52ee5c7
fallenangelx <fallen.angel.pp@gmail.com>
2015-06-30 11:43:46 -0300

    tirando da solução códigos desncecessários

--- BubbleSort.c
+++ BubbleSort.c
@@ -3,40 +3,41 @@
 int* BubbleSort(int data[], size_t count)
 {
 	int swap;
-	// sÃ³ pra testar se estÃ¡ pegando o tamanho do vetor
+	// só pra testar se está pegando o tamanho do vetor
 	printf("%d\n", count);
-	
-	for (int i = 0; i < count-1; i++)
+
+	for (int i = 0; i < count - 1; i++)
 	{
-		for (int j = 0; j < count-i-1; j++)
+		for (int j = 0; j < count - i - 1; j++)
 		{
-			if (data[j] > data[j+1])
+			if (data[j] > data[j + 1])
 			{
 				swap = data[j];
-				data[j] = data[j+1];
-				data[j+1] = swap;
+				data[j] = data[j + 1];
+				data[j + 1] = swap;
 			}
 		}
 	}
-	
+
 	return data;
 }
 
-// main apenas para testar se o sort funciona... e estÃ¡ funcionando
-void main()
+// main apenas para testar se o sort funciona... e está funcionando
+void BubbleTest()
 {
-	int arrayList[] = {10,5,3,1,7,2,4,0,11,20,3};
+	int arrayList[] = { 10, 5, 3, 1, 7, 2, 4, 0, 11, 20, 3 };
 
 	for (int i = 0; i < (sizeof arrayList / sizeof *arrayList); i++)
 	{
 		printf("%d ", arrayList[i]);
 	}
-	
+
 	// (sizeof arrayList / sizeof *arrayList) essa parada pega o tamanho do vetor
 	BubbleSort(arrayList, sizeof arrayList / sizeof *arrayList);
-	
+
 	for (int i = 0; i < (sizeof arrayList / sizeof *arrayList); i++)
 	{
 		printf("%d ", arrayList[i]);
 	}
+	getchar();
 }
